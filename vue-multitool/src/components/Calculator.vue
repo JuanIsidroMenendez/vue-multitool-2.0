13c92e1324b12fba2a10f70bb96ad0ef528434ad
Juan <juan.isidro.menendez@gmail.com>
2026-07-14 14:32:13 +0200

    feat: Calculator.vue

---
+++ vue-multitool/src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { operate } from '@/utils/operate';
+
+const {
+    display,
+    inputDigital,
+    inputDecimal,
+    chooseOperator, //Diferencia entre chooseOperator e inputOperator?
+    calculate,
+    equals,
+    clear,
+}
+
+</script>
