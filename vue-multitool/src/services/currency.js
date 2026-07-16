6fa09b9c692d335d84e7826425be6a72b7109cd7
Juan <juan.isidro.menendez@gmail.com>
2026-07-16 12:27:33 +0200

    feat: update currency.js

--- vue-multitool/src/services/currency.js
+++ vue-multitool/src/services/currency.js
@@ -1,14 +1,27 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API_URL = 'https://api.currencyfreaks.com/v2.0/rates/latest';  
-const API_KEY = import.meta.env.VITE_CURRENCY_API_KEY;
+const API_URL = 'https://api.currencyfreaks.com/v2.0/rates/latest'
+const API_KEY = import.meta.env.VITE_CURRENCY_API_KEY
+const SUPPORTED = ['EUR', 'USD', 'JPY']
+
+function mapRatesResponse(data) {
+  const rates = {}
+  for (const code of SUPPORTED) {
+    rates[code] = parseFloat(data.rates[code])
+  }
+  return {
+    base: data.base,
+    updatedAt: data.date,
+    rates
+  }
+}
 
 export async function getCurrencyRates() {
   const response = await axios.get(API_URL, {
     params: {
       apikey: API_KEY,
-      symbols: 'EUR,JPY,USD'
+      symbols: SUPPORTED.join(',')
     }
   })
-  return response.data.rates
+  return mapRatesResponse(response.data)
 }
