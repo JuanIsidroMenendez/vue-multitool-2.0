2eebeceed4e10a2b83c0ef4e650a86bee4aeb56b
Juan <juan.isidro.menendez@gmail.com>
2026-07-17 09:18:16 +0200

    chore: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api-tiempo': {
+        target: 'https://www.el-tiempo.net',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api-tiempo/, '/api/json/v2'),
+        headers: {
+          'Referer': 'https://www.el-tiempo.net/',
+          'User-Agent': 'Mozilla/5.0'
+        }
+      }
+    }
+  }
 })
