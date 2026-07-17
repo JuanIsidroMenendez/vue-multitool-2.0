6172bb62d14e3bf96cd5a8c9ef86398ae106d482
Juan <juan.isidro.menendez@gmail.com>
2026-07-17 14:00:06 +0200

    fix: changing e2e location and fixing errors

--- test/calculator.spec.test.js
+++ test/calculator.spec.test.js
@@ -4,7 +4,7 @@ test('suma dos números y muestra el resultado', async ({ page }) => {
   await page.goto('/')
 
   await page.getByRole('button', { name: '7' }).click()
-  await page.getByRole('button', { name: '+' }).click()
+  await page.getByRole('button', { name: '+', exact: true }).click()
   await page.getByRole('button', { name: '3' }).click()
   await page.getByRole('button', { name: '=' }).click()
 
